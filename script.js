d76202cf9593ae2ec8306ea726b9351049e28680
SriMoorthy <srimoorthy27@gmail.com>
2024-07-13 15:27:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+
+$input = document.getElementById('band')
+
+for(let i  = 0 ; i < bands.length ; i++){
+    $list = document.createElement('li')
+    $list.innerHTML = bands[i]
+    $input.appendChild($list)
+}
